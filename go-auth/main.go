035bd58c5cbf282389740bd71f74cb347c243de2
egosy <65727224+egosy@users.noreply.github.com>
2024-03-23 23:36:36 +0300

    Add files via upload

---
+++ go-auth/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	middleware "go-auth/middleware"
+	routes "go-auth/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+
+	router.Use(middleware.Authentication())
+
+	// API-1
+	router.GET("/api-1", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+	})
+
+	// API-2
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
